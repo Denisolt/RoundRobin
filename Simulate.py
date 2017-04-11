195c47f4b635e5107e63b8d78943db1849161995
Deni Shakh <admin@MacBook-dl.local>
2017-04-11 14:26:09 -0400

    another fix

--- Simulate.py
+++ Simulate.py
@@ -18,52 +18,53 @@ class Simulator:
         self.ProcessQueue = queue.Queue()
         self.RunQueue = queue.Queue()
 
-    def ProcessAdd(self, pid, arrival, burst):
+    def ProcessAdd(self, pid, arrival, burst, number):
         process = Process(pid, arrival, burst)
         self.list.append(process)
 
     def Check(self, clock):
         for process in self.list:
-                if process.arrival == self.clock:
-                    self.ProcessQueue.put(process)
-
-    def Scheduling(self):
-        self.timer = 0
-        while self.clock < MaxTime:
-            for process in self.list:
-                if process.arrival == self.clock:
+                if process.arrival == self.clock and number <= 3:
                     self.ProcessQueue.put(process)
                     break
 
-            if not self.ProcessQueue.empty():
-                process = self.ProcessQueue.get()
-                process.burst = process.burst - self.quantum
-                if(process.burst < 0):
-                    process.burst = 0
-                else:
-                    print '|ID:{:3d}| Start:{:3d}| End:{:3d}| Time Left:{:3d}'.format(process.pid, self.timer, self.timer + self.quantum, process.burst)
-                #print(process)
-
-            self.timer = self.timer + self.quantum
+    def Scheduling(self):
+        self.timer = 0
+        for process in self.list:
+                if process.arrival == self.clock and number <= 3:
+                    self.ProcessQueue.put(process)
+                    break
+        while self.clock < MaxTime:
+            #self.Check(self.clock)
             self.clock = self.clock + 1
             self.Check(self.clock)
-            #this should let the process + 1.arrival == self.clock and put it in the queue
-            #if process.arrival == self.clock:
-            #        self.ProcessQueue.put(process)
-            if not process.burst <= 0:
-                    self.ProcessQueue.put(process)
+            
+            if not self.ProcessQueue.empty():
+                process = self.ProcessQueue.get()
+                if(process.burst < self.quantum):
+                    self.timer = self.timer + process.burst
+                    bur = process.burst
+                    process.burst = 0
+                    print '|ID:{:3d}| Start:{:3d}| End:{:3d}| Time Left:{:3d}'.format(process.pid, self.timer, self.timer + bur, process.burst)
+                else:
+                    process.burst = process.burst - self.quantum
+                    print '|ID:{:3d}| Start:{:3d}| End:{:3d}| Time Left:{:3d}'.format(process.pid, self.timer, self.timer + self.quantum, process.burst)
+                    self.timer = self.timer + self.quantum
 
+            if not process.burst <= 0:
+                self.ProcessQueue.put(process)
 
 Simulation = Simulator()
-
+number = 0
 with open('process.csv') as csvfile:
     process = csv.reader(csvfile, delimiter=',', quotechar='|')
     next(process)
     for row in process:
+            number = number + 1
             pid=int(row[0])
             arrival = int(row[1])
             burst = int(row[2])
-            Simulation.ProcessAdd(pid,arrival,burst)
+            Simulation.ProcessAdd(pid,arrival,burst,number)
 
 print('Adding the process to the process List\n---------------------------------------------')
 for process in Simulation.list:
